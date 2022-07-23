46693d3bf519076a503aad011abdf6838b206e4a
JailsonSantos <jailson.ads007@gmail.com>
2022-07-23 13:58:52 -0300

    style: project base configuration

---
+++ src/styles/global.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme.colors['brand-yellow-dark']};
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors["base-background"]};
+    color: ${({ theme }) => theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.textSizes["text-regular-m"]}
+  }
+
+  button{ 
+    cursor: pointer;
+  }
+
+  a { 
+    text-decoration: none;
+  }
+
+`
